6bfc9bb9a26017ec05f003d391f0f7a027daa5d7
RevolutionaryAd67 <marvin.wittrisch@icloud.com>
2025-10-15 14:45:22 +0200

    Delete app directory

--- app/utils/time.py
+++
@@ -1,9 +0,0 @@
-"""Time formatting utilities."""
-from __future__ import annotations
-
-from datetime import datetime, timezone
-
-
-def utcnow_iso() -> str:
-    """Return current UTC time in ISO-8601 format."""
-    return datetime.now(timezone.utc).isoformat()
